7d053f40026a193c49a2e0d67803b8f96dfd0600
Nemo Nutchaya <nv@manaosoftware.com>
2019-06-14 18:10:15 +0700

    Add script for responsive

---
+++ scripts/shaveCustom.js
@@ -0,0 +1,133 @@
+function ShaveContent(element) {
+    var context;
+    this.body = $('body');
+    this.html = $('html');
+    this.shaveParagraph = $(element);
+    this.tabletQuery = Modernizr.mq('(min-width: 768px)');
+    this.laptopQuery = Modernizr.mq('(min-width: 992px)');
+
+    this.init = function() {
+        this.viewPort();
+        this.onScreenChange();
+        this.responsiveOptions();
+    }
+
+    this.onScreenChange = function () {
+        $(window).resize(function () {
+            context.viewPort();
+            context.responsiveOptions();
+        });
+    }
+
+    this.responsiveOptions = function () {
+        this.setLinesResponsive();
+        this.viewPort();
+        console.log(parseInt(context.responsiveMobile));
+        if(this.setLinesResponsive()) {
+            if(context.isMobile) {
+                shave(context.shaveParagraph, parseInt(context.responsiveMobile));
+                console.log('Shave Mobile max-height: ' + parseInt(this.responsiveMobile));
+            } else if (context.isTablet) {
+                shave(context.shaveParagraph, parseInt(context.responsiveTablet));
+                console.log('Shave Tablet max-height: ' + parseInt(this.responsiveTablet));
+            } else {
+                shave(context.shaveParagraph, parseInt(context.responsiveDesktop));
+                console.log('Shave Desktop max-height: ' + parseInt(this.responsiveDesktop));
+            }
+        }
+    }
+
+    this.setLinesResponsive = function() {
+        this.dataResponsiveLine = this.shaveParagraph.attr('data-responsiveLine');
+        if(this.dataResponsiveLine) {
+            var responsiveNumberLine = [];
+            responsiveNumberLine = this.dataResponsiveLine.split(",");
+            this.responsiveMobile = responsiveNumberLine[0];
+            this.responsiveTablet = responsiveNumberLine[1];
+            // this.responsiveLabtop = responsiveNumberLine[2];
+            this.responsiveDesktop = responsiveNumberLine[2];
+
+            if (this.responsiveOption) {
+                this.lineToshow = responsiveNumberLine[2];
+                //this.slideToScroll = responsiveNumberItem[3];
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    this.viewPort = function () {
+        /* Each brouser have different vertical scrollbar */
+        var scrollBarWidth = window.innerWidth - this.body.width();
+        var is_safari = navigator.userAgent.indexOf("Safari") > -1;
+        var isChrome = !!window.chrome && !!window.chrome.webstore;
+        if (!is_safari) {
+            if ($(window).width() > (992 - scrollBarWidth)) {
+                this.isDesktop = true;
+                this.isTablet = false;
+                this.isMobile = false;
+            } else if ($(window).width() > (768 - scrollBarWidth)) {
+                this.isDesktop = false;
+                this.isTablet = true;
+                this.isMobile = false;
+            } else {
+                this.isDesktop = false;
+                this.isTablet = false;
+                this.isMobile = true;
+            }
+        } else if (isChrome) {
+            if ($(window).width() > (992 - scrollBarWidth)) {
+                this.isDesktop = true;
+                this.isTablet = false;
+                this.isMobile = false;
+            } else if ($(window).width() > (768 - scrollBarWidth)) {
+                this.isDesktop = false;
+                this.isTablet = true;
+                this.isMobile = false;
+
+            } else {
+                this.isDesktop = false;
+                this.isTablet = false;
+                this.isMobile = true;
+            }
+        } else {
+            if ($(window).width() > 992) {
+                this.isDesktop = true;
+                this.isTablet = false;
+                this.isMobile = false;
+            } else if ($(window).width() > 768) {
+                this.isDesktop = false;
+                this.isTablet = true;
+                this.isMobile = false;
+            } else {
+                this.isDesktop = false;
+                this.isTablet = false;
+                this.isMobile = true;
+            }
+        }        
+        // if (context.laptopQuery) {
+        //     console.log('laptop');
+        // } else if(context.tabletQuery) {
+        //     console.log('tablet');
+        // } else {
+        //     console.log('mobile');
+        // }
+    }
+
+    context = this;
+    this.init();
+
+}
+
+var shaveParagraphs = [];
+$(window).on('load', function() {
+    shaveParagraph_index = 0;
+    $('.shaveParagraph').each(function() {
+        shaveParagraphs[shaveParagraph_index] = new ShaveContent(this);
+        shaveParagraph_index++;
+        console.log(this);
+    })
+});
+
